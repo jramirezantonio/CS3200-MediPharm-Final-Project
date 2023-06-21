a5a50fea179d78cf112d58f98f69ad0b584325c0
jramirezantonio <101155161+jramirezantonio@users.noreply.github.com>
2023-06-21 17:03:14 -0400

    Add 9 out 10 procedures

--- pythonenv.py
+++ pythonenv.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import logging
 import pymysql
@@ -12,37 +12,40 @@ try:
         host='localhost',
         user=u,
         password=p,
-        database='sharkdb',
+        database='medipharm',
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor,
         autocommit=True
     )
-except pymysql.err.OperationalError as e:
-    print('Error: %d: %s' % (e.args[0], e.args[1]))
+except pymysql.Error as e:
+    code, msg = e.args
+    print("Cannot connect to the database ", code, msg)
 
 
 def work():
     try:
-        t = input('Enter town: ')
-        s = input('Enter state: ')
-
+        # frupobu93iP_
         c1 = connection.cursor()
-        query = 'SELECT * FROM township WHERE `town` = %s AND `state` = %s'
-        df1 = pd.read_sql(query, connection, params=(t, s))
-        print(df1)
+        # query = 'SELECT * FROM drug'
+        # df1 = pd.read_sql(query, connection)
+        # print(df1)
+        # c1.close()
 
-        if df1.empty:
-            print('This town and state does not exist in this database. Reenter.')
-            print('This is the list of legitimate towns and states in this database.')
-            c3 = connection.cursor()
-            query = 'SELECT town, state FROM township'
-            c3.execute(query)
-            for row in c3.fetchall():
-                print(row)
-            work()
-        else:
-            all_receivers(t, s)
-            c1.close()
+        # Working procedures/helpers:
+        # new_tuple_drug([4, 'Amphetamine', '(RS)-2-phenylpropan-2-amine', 'stimulant', '25.00', 'Schedule III', '100',
+        #                 'Apex Solutions', '10.00', 1])
+        # del_tuple_drug(4)
+        # return_drug_stock()
+        # get_cust_receipt(1)
+        # get_supp_receipt(100)
+        # get_invoice('employee', 1)
+        # update_cust_info([1, 'mobile', '9086738912'])
+        # update_drug_quant(2, -100)
+
+        # Python doesn't like ints that start with a zero so changed zipcode to a string instead
+        add_address([40, 'Tuttle Street', 'Boston', 'MA', '02126'])
+
+        c1.close()
 
     except pymysql.Error as e:
         print('Error: %d: %s' % (e.args[0], e.args[1]))
@@ -53,13 +56,90 @@ def work():
             print('Closing connection.')
 
 
-def all_receivers(t, s):
+def new_tuple_drug(params):
     c2 = connection.cursor()
-    c2.callproc('allReceivers', (t, s))
-    print('Obtaining receivers now...')
+    c2.callproc('NewTupleDrug', tuple((par for par in params)))
+    print("Adding tuple now...")
     for row in c2.fetchall():
         print(row)
-    c2.close()
+        c2.close()
+
+
+def del_tuple_drug(id):
+    c3 = connection.cursor()
+    c3.callproc('DelTupleDrug', (id, ))
+    print("Deleting tuple now...")
+    for row in c3.fetchall():
+        print(row)
+        c3.close()
+
+
+def return_drug_stock():
+    c4 = connection.cursor()
+    c4.callproc('returnDrugStock')
+    print("Returning all drugs and their stock...")
+    for row in c4.fetchall():
+        print(row)
+        c4.close()
+
+
+def get_cust_receipt(id):
+    c5 = connection.cursor()
+    c5.callproc('getCustomerReceipt', (id, ))
+    print("Returning customer receipt...")
+    for row in c5.fetchall():
+        print(row)
+        c5.close()
+
+
+def get_supp_receipt(id):
+    c6 = connection.cursor()
+    c6.callproc('getSupplierReceipt', (id,))
+    print("Returning supplier receipt...")
+    for row in c6.fetchall():
+        print(row)
+        c6.close()
+
+
+def get_invoice(type, id):
+    c7 = connection.cursor()
+    c7.callproc('getInvoice', (type, id))
+    print("Returning invoice...")
+    if c7.fetchall()[0]['message'] == 'Customer Invoice Selected':
+        get_cust_receipt(id)
+        c7.close()
+    elif c7.fetchall()[0]['message'] == 'Supplier Invoice Selected':
+        get_supp_receipt(id)
+        c7.close()
+
+
+def update_cust_info(params):
+    c8 = connection.cursor()
+    c8.callproc('updateCustomerInfo', tuple((par for par in params)))
+    print("Updating customer info...")
+    for row in c8.fetchall():
+        print(row)
+        c8.close()
+
+
+def update_drug_quant(id, quant):
+    c9 = connection.cursor()
+    c9.callproc('updateDrugQuantity', (id, quant))
+    print("Updating drug quantity...")
+    for row in c9.fetchall():
+        print(row)
+        c9.close()
+
+
+def add_address(params):
+    c10 = connection.cursor()
+    c10.callproc('AddAddress', tuple((par for par in params)))
+    print("Adding address...")
+    for row in c10.fetchall():
+        print(row)
+        c10.close()
+
+
 
 
 if __name__ == '__main__':
